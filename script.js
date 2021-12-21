72ae5a244e651a0f62cbaa3c0cdc82aaf84a1d06
regina404 <63124387+regina404@users.noreply.github.com>
2021-12-21 09:56:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// function clickAd(){
+//     let root = document.querySelector('#ourWork');
+//     let newRow = document.createElement('div');
+//     newRow.className = 'work';
+//     newRow.innerHTML = `<div class="work">
+//     <a class="lab" href="#">Опіс предметного середовіща</a>
+//     <a class="lab" href="#">Тема Мета Розташування лаби</a>
+//     <a class="lab" href="#">Структура Документа</a>
+//     <a class="lab" id="tableCode" href="#">HTML-код таблиць</a>
+//     <a class="lab" id="formCode" href="#">HTML-код форми</a>
+//     <a class="lab" id="imgCode" href="#">HTML-код зображення</a>
+//     <a class="lab" href="#">Висновки</a>
+// </div>
+// <div class="output">
+//     <div class="outputLab"></div>
+// </div>`;
+//     root.append(newRow);
+//     console.log(root);
+// }
+
+function showLab1() {
+    let labBlock = document.getElementById('ourWork');
+    if(labBlock.style.display !== 'none') {
+        labBlock.style.display = 'none';
+    }else{
+        labBlock.style.display = 'block';
+    }
+}
+
+function showLab2() {
+    let labBlock2 = document.getElementById('ourWork2');
+    if(labBlock2.style.display !== 'none') {
+        labBlock2.style.display = 'none';
+    }else{
+        labBlock2.style.display = 'block';
+    }
+}
+
+function showLab3() {
+    let labBlock3 = document.getElementById('ourWork3');
+    if(labBlock3.style.display !== 'none') {
+        labBlock3.style.display = 'none';
+    }else{
+        labBlock3.style.display = 'block';
+    }
+}
+
+function showLab4() {
+    let labBlock4 = document.getElementById('ourWork4');
+    if(labBlock4.style.display !== 'none') {
+        labBlock4.style.display = 'none';
+    }else{
+        labBlock4.style.display = 'block';
+    }
+}
+
+function showLab5() {
+    let labBlock5 = document.getElementById('ourWork5');
+    if(labBlock5.style.display !== 'none') {
+        labBlock5.style.display = 'none';
+    }else{
+        labBlock5.style.display = 'block';
+    }
+}
+
+function showLab6() {
+    let labBlock6 = document.getElementById('ourWork6');
+    if(labBlock6.style.display !== 'none') {
+        labBlock6.style.display = 'none';
+    }else{
+        labBlock6.style.display = 'block';
+    }
+}
+function showLab7() {
+    let labBlock7 = document.getElementById('ourWork7');
+    if(labBlock7.style.display !== 'none') {
+        labBlock7.style.display = 'none';
+    }else{
+        labBlock7.style.display = 'block';
+    }
+}
+
+function showLab8() {
+    let labBlock8 = document.getElementById('ourWork8');
+    if(labBlock8.style.display !== 'none') {
+        labBlock8.style.display = 'none';
+    }else{
+        labBlock8.style.display = 'block';
+    }
+}
+
+function showLab9() {
+    let labBlock9 = document.getElementById('ourWork9');
+    if(labBlock9.style.display !== 'none') {
+        labBlock9.style.display = 'none';
+    }else{
+        labBlock9.style.display = 'block';
+    }
+}
